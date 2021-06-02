c3ee80e41b8d733a03cf02dc216cc6843df56eb3
richardthorp <richardthorp@hotmail.co.uk>
2021-06-02 14:50:08 +0000

    Add login required decorator and add if statement to CRUD views

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 from .models import UserProfile
@@ -7,6 +8,7 @@ from checkout.models import Order
 from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
