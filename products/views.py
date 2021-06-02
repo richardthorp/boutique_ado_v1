0cb70a8969ea13ffab40e28b986c3f2a3bb1ee9c
richardthorp <richardthorp@hotmail.co.uk>
2021-06-02 14:42:10 +0000

    Add edit and delete url and view

--- products/views.py
+++ products/views.py
@@ -67,9 +67,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Error with form data!')
     else:
@@ -103,3 +103,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted')
+
+    return redirect(reverse('products'))
