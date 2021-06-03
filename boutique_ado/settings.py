d486121589aefab4d6d9466e53f720fcee847827
richardthorp <richardthorp@hotmail.co.uk>
2021-06-03 05:34:14 +0000

    Change allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-htro7ucl^-4@*b(ce3+w!ftdk)cvxxw$y@255d(%r8uoew1qig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://rich-boutique.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['rich-boutique.herokuapp.com', 'localhost']
 
 
 # Application definition
