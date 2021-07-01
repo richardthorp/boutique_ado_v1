695542ea594287e367575d4953b207112b25330d
richardthorp <richardthorp@hotmail.co.uk>
2021-07-01 07:48:30 +0000

    Fix checkout 500 error

--- checkout/views.py
+++ checkout/views.py
@@ -162,9 +162,9 @@ def checkout_success(request, order_number):
             'default_country': order.country,
         }
 
-    user_profile_form = UserProfileForm(profile_data, instance=profile)
-    if user_profile_form.is_valid():
-        user_profile_form.save()
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
